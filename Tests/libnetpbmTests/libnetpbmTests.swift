30c932304eeb851fb06de928e53672e286ee154e
Valeriy Van <github@w7software.com>
2024-05-26 23:53:43 +0300

    Rename test case

--- Tests/libnetpbmTests/libnetpbmTests.swift
+++ Tests/libnetpbmTests/libnetpbmTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import libnetpbm
 
-final class swift_netpbmTests: XCTestCase {
+final class libnetpbmTests: XCTestCase {
 
     func testExample() throws {
         // XCTest Documentation
@@ -10,4 +10,5 @@ final class swift_netpbmTests: XCTestCase {
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
+
 }
