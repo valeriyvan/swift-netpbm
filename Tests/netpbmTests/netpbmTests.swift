39cf7f38f4f2924f6d187beef8f5730d66af257c
Valeriy Van <github@w7software.com>
2024-05-27 09:46:41 +0300

    Rename functions

--- Tests/netpbmTests/netpbmTests.swift
+++ Tests/netpbmTests/netpbmTests.swift
@@ -9,7 +9,7 @@ import XCTest
 final class netpbmTests: XCTestCase {
 
     func testReadFirstImageFromString() throws {
-        let (rows, cols, pixels) = try PBM.readFirstImage(string:
+        let (rows, cols, pixels) = try PBM.firstImage(string:
             """
             P1
             # Image 1: 5x4 checkerboard pattern
