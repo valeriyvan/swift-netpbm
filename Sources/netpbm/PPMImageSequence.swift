38b8d46b06bad6d7a33c6befea34a945281e46bf
Valeriy Van <github@w7software.com>
2024-06-09 23:38:53 +0300

    Port reading and writing PPM images

---
+++ Sources/netpbm/PPMImageSequence.swift
@@ -0,0 +1,340 @@
+import Foundation
+import Algorithms // chunks
+
+public struct PPMImageSequence: AsyncSequence {
+
+    public typealias Element = PPMImagePixelSequence
+    public typealias AsyncIterator = Iterator
+
+    public struct Iterator: AsyncIteratorProtocol {
+        public let file: UnsafeMutablePointer<FILE>
+
+        public init(file: UnsafeMutablePointer<FILE>) {
+            self.file = file
+        }
+
+        public mutating func next() async throws -> PPMImagePixelSequence? {
+            let eof = try _pm_nextimage(file)
+            guard !eof else { return nil }
+            return try PPMImagePixelSequence(file: file)
+        }
+    }
+
+    public var file: UnsafeMutablePointer<FILE> { fileWrapper.file }
+
+    let fileWrapper: FileWrapper
+
+    public init(data: Data) throws {
+        let (file, buffer) = try data.withUnsafeBytes {
+            // Make a copy of $0 otherwise file operations will read deallocated memory
+            let buffer = UnsafeMutableRawPointer.allocate(byteCount: $0.count, alignment: MemoryLayout<UInt8>.alignment)
+            buffer.copyMemory(from: $0.baseAddress!, byteCount: $0.count)
+            guard let file: UnsafeMutablePointer<FILE> = fmemopen(buffer, $0.count, "r") else {
+                throw NSError(domain: URLError.errorDomain, code: URLError.cannotOpenFile.rawValue)
+            }
+            return (file, buffer)
+        }
+        self.fileWrapper = FileWrapper(file: file, buffer: buffer)
+    }
+
+    public init(pathname: String) throws {
+        guard let file: UnsafeMutablePointer<FILE> = fopen(pathname, "r") else {
+            throw NSError(domain: URLError.errorDomain, code: URLError.cannotOpenFile.rawValue)
+        }
+        self.fileWrapper = FileWrapper(file: file)
+    }
+
+    public func makeAsyncIterator() -> Iterator {
+        Iterator(file: file)
+    }
+}
+
+public struct PPMImagePixelSequence: AsyncSequence {
+    public typealias Element = Pixel
+    public typealias AsyncIterator = Iterator
+
+    public struct Iterator: AsyncIteratorProtocol {
+        var currentRow = -1
+        var row: [Pixel] = []
+        var currentElementIndex = 0
+        let cols: Int32
+        let rows: Int32
+        let maxVal: Pixval
+        let format: Int32
+        let file: UnsafeMutablePointer<FILE>
+
+        init(file: UnsafeMutablePointer<FILE>, cols: Int32, rows: Int32, maxVal: Pixval, format: Int32) {
+            self.file = file
+            self.cols = cols
+            self.rows = rows
+            self.maxVal = maxVal
+            self.format = format
+        }
+
+        public mutating func next() async throws -> Pixel? {
+            if currentRow == -1 {
+                row = try _ppm_readppmrow(file, cols: cols, maxVal: maxVal, format: format)
+                currentRow = 0
+                currentElementIndex = 0
+                defer { currentElementIndex += 1 }
+                return row[currentElementIndex]
+            } else if currentElementIndex < row.count {
+                defer { currentElementIndex += 1 }
+                return row[currentElementIndex]
+            } else if currentRow < rows-1 {
+                row = try _ppm_readppmrow(file, cols: cols, maxVal: maxVal, format: format)
+                currentRow += 1
+                currentElementIndex = 0
+                defer { currentElementIndex += 1 }
+                return row[currentElementIndex]
+            }
+            return nil
+        }
+    }
+
+    public var width: Int { Int(cols) }
+    public var height: Int { Int(rows) }
+    public var maxValue: Pixval { maxVal }
+
+    private let file: UnsafeMutablePointer<FILE>
+    private let cols: Int32
+    private let rows: Int32
+    private let maxVal: Pixval
+    private let format: Int32
+
+    public init(file: UnsafeMutablePointer<FILE>) throws {
+        self.file = file
+        (cols, rows, maxVal, format) = try _ppm_readppminit(file)
+    }
+
+    public func makeAsyncIterator() -> Iterator {
+        return Iterator(file: file, cols: cols, rows: rows, maxVal: maxVal, format: format)
+    }
+}
+
+public enum PpmParseError: Error {
+    case wrongFormat // header is wrong
+    case ioError
+//    case internalInconsistency
+    case insufficientMemory
+    case unexpectedEndOfFile
+//    case junkWhereBitsShouldBe
+//    case junkWhereUnsignedIntegerShouldBe
+//    case tooBigNumber
+    case imageTooLarge
+    case badPixelValue
+}
+
+let PPM_OVERALLMAXVAL = PGM_OVERALLMAXVAL
+let PPM_MAXMAXVAL = PGM_MAXMAXVAL
+
+public typealias Pixval = Gray
+
+public struct Pixel: Equatable {
+    var r: Pixval
+    var g: Pixval
+    var b: Pixval
+}
+
+func _ppm_readppminit(_ file: UnsafeMutablePointer<FILE>) throws -> (cols: Int32, rows: Int32, maxVal: Pixval, format: Int32) {
+    /* Check magic number. */
+    let realFormat = try _pm_readmagicnumber(file)
+    var cols: Int32 = 0, rows: Int32 = 0
+    var maxVal: Pixval = 0
+    var format: Int32
+    switch PAM_FORMAT_TYPE(realFormat) {
+    case PPM_TYPE:
+        format = realFormat
+        (cols, rows, maxVal) = try _ppm_readppminitrest(file)
+    case PGM_TYPE:
+        format = realFormat
+        (cols, rows, maxVal) = try _pgm_readpgminitrest(file)
+    case PBM_TYPE:
+        format = realFormat
+        /* See comment in pgm_readpgminit() about this maxval */
+        maxVal = PPM_MAXMAXVAL
+        (cols, rows) = try _pbm_readpbminitrest(file)
+    case PAM_TYPE:
+        (cols, rows, maxVal, format) = try _pnm_readpaminitrestaspnm(file)
+    default:
+        print("bad magic number \(String(format: "0x%x", realFormat)) - not a PPM, PGM, PBM, or PAM file")
+        throw PpmParseError.wrongFormat
+    }
+    try _ppm_validateComputableSize(cols: cols, rows: rows)
+
+    try _pgm_validateComputableMaxval(maxVal: maxVal)
+
+    return (cols: cols, rows: rows, maxVal: maxVal, format: format)
+}
+
+func _ppm_readppminitrest(_ file: UnsafeMutablePointer<FILE>) throws -> (cols: Int32, rows: Int32, maxVal: Pixval) {
+    return try _pgm_readpgminitrest(file)
+}
+
+func _ppm_validateComputableSize(cols: Int32, rows: Int32) throws {
+/*----------------------------------------------------------------------------
+   Validate that the dimensions of the image are such that it can be
+   processed in typical ways on this machine without worrying about
+   overflows.  Note that in C, arithmetic is always modulus
+   arithmetic, so if your values are too big, the result is not what
+   you expect.  That failed expectation can be disastrous if you use
+   it to allocate memory.
+
+   It is very normal to allocate space for a pixel row, so we make sure
+   the size of a pixel row, in bytes, can be represented by an 'int'.
+
+   A common operation is adding 1 or 2 to the highest row or
+   column number in the image, so we make sure that's possible.
+-----------------------------------------------------------------------------*/
+    if cols > Int32.max / (Int32(MemoryLayout<Pixval>.size) * 3) || cols > Int32.max - 2 {
+        print("image width (\(cols) too large to be processed")
+        throw PpmParseError.imageTooLarge
+    }
+    if rows > Int32.max - 2 {
+        print("image height (\(rows) too large to be processed")
+        throw PpmParseError.imageTooLarge
+    }
+}
+
+func _ppm_readppmrow(_ file: UnsafeMutablePointer<FILE>, cols: Int32, maxVal: Pixval, format: Int32) throws -> [Pixel] {
+    switch (format) {
+    case PPM_FORMAT:
+        return try _readPpmRow(file, cols: cols, maxVal: maxVal, format: format)
+    /* For PAM, we require a depth of 3, which means the raster format
+       is identical to Raw PPM!  How convenient.
+    */
+    case PAM_FORMAT:
+        fallthrough
+    case RPPM_FORMAT:
+        return try _readRppmRow(file, cols: cols, maxVal: maxVal, format: format)
+    case PGM_FORMAT:
+        fallthrough
+    case RPGM_FORMAT:
+        fatalError("Not implemented")
+        // return try _readPgmRow(file, cols: cols, maxVal: maxVal, format: format)
+    case PBM_FORMAT:
+        fallthrough
+    case RPBM_FORMAT:
+        fatalError("Not implemented")
+        // return try _readPbmRow(file, cols: cols, maxVal: maxVal, format: format)
+    default:
+        print("Invalid format code")
+        throw PpmParseError.wrongFormat
+    }
+}
+
+func _readPpmRow(_ file: UnsafeMutablePointer<FILE>, cols: Int32, maxVal: Pixval, format: Int32) throws -> [Pixel] {
+    var row: [Pixel] = []
+    row.reserveCapacity(Int(cols))
+    for _ in 0..<Int(cols) {
+        let r: Pixval = try _pm_getuint(file)
+        let g: Pixval = try _pm_getuint(file)
+        let b: Pixval = try _pm_getuint(file)
+        if r > maxVal {
+            print("Red sample value \(r) is greater than maxval (\(maxVal)")
+            throw PpmParseError.wrongFormat // ???
+        }
+        if g > maxVal {
+            print("Green sample value \(g) is greater than maxval (\(maxVal)")
+            throw PpmParseError.wrongFormat // ???
+        }
+        if b > maxVal {
+            print("Blue sample value \(b) is greater than maxval (\(maxVal)")
+            throw PpmParseError.wrongFormat // ???
+        }
+        row.append(Pixel(r: r, g: g, b: b))
+    }
+    return row
+}
+
+func _readRppmRow(_ file: UnsafeMutablePointer<FILE>, cols: Int32, maxVal: Pixval, format: Int32) throws -> [Pixel] {
+    let bytesPerSample = maxVal < 256 ? 1 : 2
+    let bytesPerRow = Int(cols) * 3 * bytesPerSample
+
+    let rowBuffer = malloc(bytesPerRow)
+    guard let rowBuffer else {
+        print("Unable to allocate memory for row buffer for \(cols) columns")
+        throw PpmParseError.insufficientMemory
+    }
+    defer { free(rowBuffer) }
+
+    let rc = fread(rowBuffer, 1, bytesPerRow, file)
+    guard feof(file) != EOF else {
+        print("Unexpected EOF reading row of PPM image.")
+        throw PpmParseError.unexpectedEndOfFile
+    }
+    guard ferror(file) == 0 else {
+        print("Error reading row.  fread() errno=\(errno) (\(String(cString: strerror(errno)))")
+        throw PpmParseError.ioError
+    }
+
+    let bytesRead = rc
+    guard bytesRead == bytesPerRow else {
+        print("Error reading row.  Short read of \(bytesRead) bytes instead of \(bytesPerRow)")
+        throw PpmParseError.ioError
+    }
+    let row = try _interpRasterRowRaw(rowBuffer: rowBuffer, cols: cols, bytesPerSample: bytesPerSample)
+    try _validateRppmRow(row: row, cols: cols, maxVal: maxVal)
+    return row
+}
+
+func _interpRasterRowRaw (rowBuffer: UnsafeMutableRawPointer, cols: Int32, bytesPerSample: Int) throws -> [Pixel] {
+    if bytesPerSample == 1 {
+        return UnsafeBufferPointer<UInt8>(
+                start: rowBuffer.bindMemory(to: UInt8.self, capacity: 1),
+                count: Int(cols) * 3
+            )
+            .chunks(ofCount: 3)
+            .map {
+                Pixel(
+                    r: Pixval($0[$0.startIndex]), 
+                    g: Pixval($0[$0.startIndex.advanced(by: 1)]),
+                    b: Pixval($0[$0.startIndex.advanced(by: 2)])
+                )
+            }
+    } else  {
+        /* two byte samples */
+        return UnsafeBufferPointer<UInt16>(
+                start: rowBuffer.bindMemory(to: UInt16.self, capacity: 1),
+                count: Int(cols) * 3
+            )
+            .chunks(ofCount: 3)
+            .map {
+                Pixel(
+                    r: Pixval($0[0]),
+                    g: Pixval($0[1]),
+                    b: Pixval($0[2]))
+            }
+    }
+}
+
+func _validateRppmRow(row: [Pixel], cols: Int32, maxVal: Pixval) throws {
+/*----------------------------------------------------------------------------
+  Check for sample values above maxval in input.
+
+  Note: a program that wants to deal with invalid sample values itself can
+  simply make sure it uses a sufficiently high maxval on the read function
+  call, so this validation never fails.
+-----------------------------------------------------------------------------*/
+    if maxVal == 255 || maxVal == 65535 {
+        /* There's no way a sample can be invalid, so we don't need to look at
+           the samples individually.
+        */
+        return
+    } else {
+        for pixel in row {
+            if pixel.r > maxVal {
+                print("Red sample value \(pixel.r) is greater than maxval (\(maxVal))")
+                throw PpmParseError.badPixelValue
+            }
+            else if pixel.g > maxVal {
+                print("Green sample value \(pixel.g) is greater than maxval (\(maxVal))")
+                throw PpmParseError.badPixelValue
+            }
+            else if pixel.b > maxVal {
+                print("Blue sample value \(pixel.b) is greater than maxval (\(maxVal))")
+                throw PpmParseError.badPixelValue
+            }
+        }
+    }
+}
---
+++ Sources/netpbm/PPMImageWriter.swift
@@ -0,0 +1,158 @@
+import Foundation
+
+public enum PpmWriteError: Error {
+    case ioError
+    case wrongMaxVal
+}
+
+public struct PPMImageWriter {
+    // In case of plane output returned value will be data of String in .ascii encoding
+    // (.utf8, obviously, works as well).
+    // In case of raw (binary) output, Data returned shouldn't be used for constructing String
+    // as encoding cannot be defined.
+    public static func write(images: [(cols: Int, rows: Int, maxValue: Pixval, pixels: [Pixel])], forcePlane: Bool) throws -> Data {
+        assert(images.allSatisfy { image in image.pixels.allSatisfy { $0.r <= image.maxValue && $0.g <= image.maxValue && $0.b <= image.maxValue } })
+        guard let tmpUrl = createTemporaryFile() else {
+            throw PpmWriteError.ioError
+        }
+        try write(images: images, pathname: tmpUrl.path, forcePlane: forcePlane)
+        let data = try Data(contentsOf: tmpUrl)
+        try FileManager.default.removeItem(at: tmpUrl)
+        return data
+    }
+
+    public static func write(images: [(cols: Int, rows: Int, maxValue: Pixval, pixels: [Pixel])], pathname: String, forcePlane: Bool) throws {
+        guard let file: UnsafeMutablePointer<FILE> = fopen(pathname, "w") else {
+            throw NSError(domain: URLError.errorDomain, code: URLError.cannotOpenFile.rawValue)
+        }
+        try write(images: images, file: file, forcePlane: forcePlane)
+        guard fclose(file) != EOF else {
+            throw PpmWriteError.ioError
+        }
+    }
+
+    static func write(images: [(cols: Int, rows: Int, maxValue: Pixval, pixels: [Pixel])], file: UnsafeMutablePointer<FILE>, forcePlane: Bool) throws {
+        let imagesCount = images.count
+        for (i, image) in images.enumerated() {
+            try _ppm_writeppm(
+                file,
+                pixels: image.pixels,
+                cols: Int32(image.cols), rows: Int32(image.rows),
+                maxVal: image.maxValue,
+                forceplain: forcePlane
+            )
+            if i < imagesCount - 1 { // TODO: only do this in plane text mode?
+                guard putc(Int32(Character("\n").asciiValue!), file) != EOF else {
+                    throw PpmWriteError.ioError
+                }
+            }
+        }
+    }
+
+}
+
+func _ppm_writeppm(_ file: UnsafeMutablePointer<FILE>, pixels: [Pixel], cols: Int32, rows: Int32, maxVal: Pixval, forceplain: Bool) throws {
+    try _ppm_writeppminit(file, cols: cols, rows: rows, maxVal: maxVal, forceplain: forceplain)
+    for row in 0..<Int(rows) {
+        try _ppm_writeppmrow(file, pixelrow: pixels[row * Int(cols) ..< (row + 1) * Int(cols)], maxVal: maxVal, forceplain: forceplain)
+    }
+}
+
+func _ppm_writeppmrow(_ file: UnsafeMutablePointer<FILE>, pixelrow: ArraySlice<Pixel>, maxVal: Pixval, forceplain: Bool) throws {
+    if forceplain || maxVal >= 1<<16 {
+        try _ppm_writeppmrowplain(file, pixelrow: pixelrow, maxVal: maxVal)
+    } else {
+        try _ppm_writeppmrowraw(file, pixelrow: pixelrow, maxVal: maxVal)
+    }
+}
+
+func _ppm_writeppminit(_ file: UnsafeMutablePointer<FILE>, cols: Int32, rows: Int32, maxVal: Pixval, forceplain: Bool) throws {
+    let plainFormat = forceplain
+    /* For Caller's convenience, we include validating computability of the
+       image parameters, since Caller may be using them in arithmetic after
+       our return.
+    */
+    try _ppm_validateComputableSize(cols: cols, rows: rows)
+    try _pgm_validateComputableMaxval(maxVal: maxVal)
+    if maxVal > PPM_OVERALLMAXVAL && !plainFormat {
+        print("Too-large maxval passed to ppm_writeppminit(): \(maxVal). Maximum allowed by the PPM format is \(PPM_OVERALLMAXVAL).")
+        throw PpmWriteError.wrongMaxVal
+    }
+    let magic = String(format: "%c%c\n%d %d\n%d\n", PPM_MAGIC1, plainFormat || maxVal >= 1<<16 ? PPM_MAGIC2 : RPPM_MAGIC2, cols, rows, maxVal)
+    guard magic.withCString({ fputs($0, file) }) != EOF else {
+        throw PpmWriteError.ioError
+    }
+}
+
+
+func _ppm_writeppmrowraw(_ file: UnsafeMutablePointer<FILE>, pixelrow: ArraySlice<Pixel>, maxVal: Pixval) throws {
+    let buffer: UnsafeRawBufferPointer = maxVal < 256 ?
+    _format1bpsRow(pixelrow: pixelrow) : _format2bpsRow(pixelrow: pixelrow)
+    defer { buffer.deallocate() }
+    guard fwrite(buffer.baseAddress!, 1, buffer.count, file) == buffer.count else {
+        print("Error writing row. fwrite() errno=\(errno) (\(String(cString: strerror(errno)))")
+        throw PpmWriteError.ioError
+    }
+}
+
+func _ppm_writeppmrowplain(_ file: UnsafeMutablePointer<FILE>, pixelrow: ArraySlice<Pixel>, maxVal: Pixval) throws {
+    var row = ""
+    var lineCount = 0 // To avoid loop going quadratic
+    for pixel in pixelrow {
+        let strPixel = "\(pixel.r) \(pixel.g) \(pixel.b)"
+        let strPixelCount = strPixel.count
+        if lineCount == 0 {
+            row += strPixel
+            lineCount += strPixelCount
+        } else if lineCount + 1 + strPixel.count < 70 {
+            row += " " + strPixel
+            lineCount += 1 + strPixelCount
+        } else {
+            row += "\n" + strPixel
+            lineCount = strPixelCount
+        }
+    }
+    if lineCount > 0 { // TODO: this check looks redundant
+        row += "\n"
+    }
+    try row.withCString {
+        guard fputs($0, file) != EOF else {
+            throw PgmParseError.ioError
+        }
+    }
+}
+
+func _format1bpsRow(pixelrow: ArraySlice<Pixel>) -> UnsafeRawBufferPointer {
+    /* single byte samples. */
+    let byteCount = pixelrow.count * 3
+    let rawPointer = UnsafeMutableRawPointer.allocate(byteCount: byteCount, alignment: MemoryLayout<UInt8>.alignment)
+    // TODO: it looks like trivial loop for buffer copying will be much more effective
+    pixelrow
+        .flatMap { [UInt8($0.r), UInt8($0.g), UInt8($0.b)] }
+        .withUnsafeBytes {
+            rawPointer.copyMemory(from: $0.baseAddress!, byteCount: $0.count)
+        }
+    return UnsafeRawBufferPointer(start: rawPointer, count: byteCount)
+}
+
+func _format2bpsRow(pixelrow: ArraySlice<Pixel>) -> UnsafeRawBufferPointer {
+    /* two byte samples. */
+    let byteCount = pixelrow.count * 3 * 2
+    let rawPointer = UnsafeMutableRawPointer.allocate(byteCount: byteCount, alignment: MemoryLayout<UInt8>.alignment)
+    // TODO: it looks like trivial loop for buffer copying will be much more effective
+    pixelrow
+        .flatMap {
+            [
+                UInt8($0.r >> 8 & 0xff),
+                UInt8($0.r & 0xff),
+                UInt8($0.g >> 8 & 0xff),
+                UInt8($0.g & 0xff),
+                UInt8($0.b >> 8 & 0xff),
+                UInt8($0.b & 0xff)
+            ]
+        }
+        .withUnsafeBytes {
+            rawPointer.copyMemory(from: $0.baseAddress!, byteCount: $0.count)
+        }
+    return UnsafeRawBufferPointer(start: rawPointer, count: byteCount)
+}
