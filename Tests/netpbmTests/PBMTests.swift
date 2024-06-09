621a1f6a7582c10d827e579fff3a2209610faa17
Valeriy Van <github@w7software.com>
2024-06-09 23:27:31 +0300

    Comment out test

--- Tests/netpbmTests/PBMTests.swift
+++ Tests/netpbmTests/PBMTests.swift
@@ -82,7 +82,6 @@ final class PBMTests: XCTestCase {
         for try await imageBitSequence in iSeqBinary {
             let pixels: [UInt8] = try await imageBitSequence.reduce(into: []) { $0.append(UInt8($1.rawValue)) }
             let image = (cols: imageBitSequence.width, rows: imageBitSequence.height, pixels: pixels)
-            print(image)
             images.append(image)
         }
 
@@ -101,5 +100,88 @@ final class PBMTests: XCTestCase {
         XCTAssertEqual(second.pixels, [0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1])
 
     }
+
+//    func testImagesFromStringMissingBitsThrows() async throws {
+//        let imageSequence = try PBMImageSequence(data:
+//            """
+//            P1
+//            # Image 1: 5x4 checkerboard pattern
+//            5 4
+//            0 1 0 1 0
+//            1 0 1 0 1
+//            0 1 0 1 0
+//            1 0 1 0
+//
+//            """
+//            .data(using: .utf8)!
+//        )
+//
+//        var images: [(cols: Int, rows: Int, pixels: [UInt8])] = []
+//
+//        for try await imageBitSequence in imageSequence {
+//            let pixels: [UInt8] = try await imageBitSequence.reduce(into: []) { $0.append(UInt8($1.rawValue)) }
+//            let image = (cols: imageBitSequence.width, rows: imageBitSequence.height, pixels: pixels)
+//            images.append(image)
+//        }
+//
+//        XCTAssertEqual(images.count, 2)
+//
+//        first = images[0]
+//        XCTAssertEqual(first.cols, 5)
+//        XCTAssertEqual(first.rows, 4)
+//        XCTAssertEqual(first.pixels.count, 5 * 4)
+//        XCTAssertEqual(first.pixels, [0,1,0,1,0, 1,0,1,0,1, 0,1,0,1,0, 1,0,1,0,1])
+//
+//        second = images[1]
+//        XCTAssertEqual(second.cols, 4)
+//        XCTAssertEqual(second.rows, 4)
+//        XCTAssertEqual(second.pixels.count, 4 * 4)
+//        XCTAssertEqual(second.pixels, [0,0,1,1, 0,0,1,1, 0,0,1,1, 0,0,1,1])
+//
+//        XCTAssertThrowsError(
+//            try PBMImageSequence(data:
+//                """
+//                P1
+//                # Image 1: 5x4 checkerboard pattern
+//                5 4
+//                0 1 0 1 0
+//                1 0 1 0 1
+//                0 1 0 1 0
+//                1 0 1 0
+//
+//                """
+//                .data(using: .utf8)!
+//            )
+//            .last
+//            .map { try await $0.reduce(into: []) { $0.append(UInt8($1.rawValue)) } }
+//        ) { error in
+//            XCTAssertEqual(error as? PBM.PbmParseError, PBM.PbmParseError.unexpectedEndOfFile)
+//        }
+
+//        XCTAssertThrowsError(
+//            try PBM.images(string:
+//                """
+//                P1
+//                # Image 1: 5x4 checkerboard pattern
+//                5 4
+//                0 1 0 1 0
+//                1 0 1 0 1
+//                0 1 0 1 0
+//                1 0 1 0
+//
+//                P1
+//                # Image 2: 4x4 vertical stripes
+//                4 4
+//                0 0 1 1
+//                0 0 1 1
+//                0 0 1 1
+//                0 0 1 1
+//
+//                """
+//                          )
+//        ) { error in
+//            XCTAssertEqual(error as? PBM.PbmParseError, PBM.PbmParseError.junkWhereBitsShouldBe)
+//        }
+//    }
 
 }
