998da3e7afcf935f21ec8964d89662ec73cc78b4
Valeriy Van <github@w7software.com>
2024-06-10 08:23:17 +0300

    Fix test

--- Tests/netpbmTests/PGMTests.swift
+++ Tests/netpbmTests/PGMTests.swift
@@ -33,7 +33,7 @@ final class PGMTests: XCTestCase {
 
         XCTAssertEqual(images.count, 1)
 
-        let first = images[0]
+        var first = images[0]
         XCTAssertEqual(first.cols, 24)
         XCTAssertEqual(first.rows, 7)
         XCTAssertEqual(first.pixels.count, 24 * 7)
@@ -72,35 +72,35 @@ final class PGMTests: XCTestCase {
             .data(using: .utf8)
         )
 
-//        let binary = try PGMImageWriter.write(images: images, forcePlane: false)
-//
-//        // Now test that binary image could be read and it's the same
-//        let iSeqBinary = try PGMImageSequence(data: binary)
-//
-//        images = []
-//
-//        for try await imageGraySequence in iSeqBinary {
-//            let pixels: [Gray] = try await imageGraySequence.reduce(into: [Gray]()) { $0.append($1) }
-//            images.append(
-//                (cols: imageGraySequence.width, rows: imageGraySequence.height, maxValue: imageGraySequence.maxValue, pixels: pixels)
-//            )
-//        }
-//
-//        XCTAssertEqual(images.count, 1)
-//
-//        first = images[0]
-//        XCTAssertEqual(first.cols, 24)
-//        XCTAssertEqual(first.rows, 7)
-//        XCTAssertEqual(first.pixels.count, 24 * 7)
-//        XCTAssertEqual(first.pixels, [
-//            0, 0,  0,  0,  0,  0,  0,  0,  0, 0,  0,  0,  0,  0,  0,  0,  0, 0,  0,  0,  0,  0,  0,  0,
-//            0, 3,  3,  3,  3,  0,  0,  7,  7, 7,  7,  0,  0, 11, 11, 11, 11, 0,  0, 15, 15, 15, 15,  0,
-//            0, 3,  0,  0,  0,  0,  0,  7,  0, 0,  0,  0,  0, 11,  0,  0,  0, 0,  0, 15,  0,  0, 15,  0,
-//            0, 3,  3,  3,  0,  0,  0,  7,  7, 7,  0,  0,  0, 11, 11, 11,  0, 0,  0, 15, 15, 15, 15,  0,
-//            0, 3,  0,  0,  0,  0,  0,  7,  0, 0,  0,  0,  0, 11,  0,  0,  0, 0,  0, 15,  0,  0,  0,  0,
-//            0, 3,  0,  0,  0,  0,  0,  7,  7, 7,  7,  0,  0, 11, 11, 11, 11, 0,  0, 15,  0,  0,  0,  0,
-//            0, 0,  0,  0,  0,  0,  0,  0,  0, 0,  0,  0,  0,  0,  0,  0,  0, 0,  0,  0,  0,  0,  0,  0
-//        ])
+        let binary = try PGMImageWriter.write(images: images, forcePlane: false)
+        try binary.write(to: URL(fileURLWithPath: "/Users/valeriyvan/Desktop/feep.ascii.p5.pgm"), options: .atomic)
+        // Now test that binary image could be read and it's the same
+        let iSeqBinary = try PGMImageSequence(data: binary)
+
+        images = []
+
+        for try await imageGraySequence in iSeqBinary {
+            let pixels: [Gray] = try await imageGraySequence.reduce(into: [Gray]()) { $0.append($1) }
+            images.append(
+                (cols: imageGraySequence.width, rows: imageGraySequence.height, maxValue: imageGraySequence.maxValue, pixels: pixels)
+            )
+        }
+
+        XCTAssertEqual(images.count, 1)
+
+        first = images[0]
+        XCTAssertEqual(first.cols, 24)
+        XCTAssertEqual(first.rows, 7)
+        XCTAssertEqual(first.pixels.count, 24 * 7)
+        XCTAssertEqual(first.pixels, [
+            0, 0,  0,  0,  0,  0,  0,  0,  0, 0,  0,  0,  0,  0,  0,  0,  0, 0,  0,  0,  0,  0,  0,  0,
+            0, 3,  3,  3,  3,  0,  0,  7,  7, 7,  7,  0,  0, 11, 11, 11, 11, 0,  0, 15, 15, 15, 15,  0,
+            0, 3,  0,  0,  0,  0,  0,  7,  0, 0,  0,  0,  0, 11,  0,  0,  0, 0,  0, 15,  0,  0, 15,  0,
+            0, 3,  3,  3,  0,  0,  0,  7,  7, 7,  0,  0,  0, 11, 11, 11,  0, 0,  0, 15, 15, 15, 15,  0,
+            0, 3,  0,  0,  0,  0,  0,  7,  0, 0,  0,  0,  0, 11,  0,  0,  0, 0,  0, 15,  0,  0,  0,  0,
+            0, 3,  0,  0,  0,  0,  0,  7,  7, 7,  7,  0,  0, 11, 11, 11, 11, 0,  0, 15,  0,  0,  0,  0,
+            0, 0,  0,  0,  0,  0,  0,  0,  0, 0,  0,  0,  0,  0,  0,  0,  0, 0,  0,  0,  0,  0,  0,  0
+        ])
     }
 
 }
