9f45b32e148df212655a70b4ada75e418c6bf782
Valeriy Van <github@w7software.com>
2024-06-19 07:52:35 +0300

    Add constants to PAM, make properties var, change type of some properties

--- Sources/netpbm/PAM.swift
+++ Sources/netpbm/PAM.swift
@@ -1,14 +1,50 @@
 import Foundation
 
-typealias pm_filepos = Int
+let PAM_BLACK = 0
+let PAM_BW_WHITE = 1
 
-typealias Sample = UInt /* unsigned long */
+let PAM_PBM_TUPLETYPE = "BLACKANDWHITE"
+let PAM_PGM_TUPLETYPE = "GRAYSCALE"
+let PAM_PPM_TUPLETYPE = "RGB"
+let PAM_PBM_ALPHA_TUPLETYPE = "BLACKANDWHITE_ALPHA"
+let PAM_PGM_ALPHA_TUPLETYPE = "GRAYSCALE_ALPHA"
+let PAM_PPM_ALPHA_TUPLETYPE = "RGB_ALPHA"
+
+let PAM_PBM_BLACK = PAM_BLACK
+let PAM_PBM_WHITE = PAM_BW_WHITE
+    /* These are values of samples in a PAM image that represents a black
+       and white bitmap image.  They are the values of black and white,
+       respectively.  For example, if you use pnm_readpamrow() to read a
+       row from a PBM file, the black pixels get returned as
+       PAM_PBM_BLACK.
+
+       PAM_PBM_BLACK and PAM_PBM_WHITE use "light on" representing black with 0 and white with 1
+       which differs from regular PBM format using "ink on".
+     */
+
+let PAM_RED_PLANE = 0
+let PAM_GRN_PLANE = 1
+let PAM_BLU_PLANE = 2
+    /* These are plane numbers for the 3 planes of a PAM image that
+       represents an RGB image (tuple type is "RGB").  So
+       if 'pixel' is a tuple returned by pnmreadpamrow(), then
+       pixel[PAM_GRN_PLANE] is the value of the green sample in that
+       pixel.
+       */
+let PAM_TRN_PLANE: UInt32 = 3
+    /* A PAM with "RGB_ALPHA" tuple type has this 4th plane
+       for transparency.  0 = transparent, maxval = opaque.
+    */
+let PAM_GRAY_TRN_PLANE: UInt32 = 1
+    /* For a "GRAYSCALE" tuple type, this is the transparency plane */
+
+public typealias Sample = UInt /* unsigned long */
     /* Regardless of the capacity of "unsigned long", a sample is always
        less than 1 << 16.  This is essential for some code to avoid
        arithmetic overflows.
     */
 
-struct Pam {
+public struct Pam {
     /* This structure describes an open PAM image file.  It consists
        entirely of information that belongs in the header of a PAM image
        and filesystem information.  It does not contain any state
@@ -43,7 +79,7 @@ struct Pam {
            output, 'plainformat' determines whether the output is the
            raw or plain format of the type given by 'format'.
            */
-    let plainformat: UInt32
+    var plainformat: Bool
         /* Logical: On output, use plain version of the format type
            indicated by 'format'.  Otherwise, use the raw version.
            (i.e., on output, the plainness information in 'format' is
@@ -58,18 +94,18 @@ struct Pam {
            This is meaningless when 'format' is PAM_FORMAT, as PAM does not
            have plain and raw variations.
         */
-    let height: Int32  /* Height of image in rows */
-    let width: Int32
+    var height: Int32  /* Height of image in rows */
+    var width: Int32
         /* Width of image in number of columns (tuples per row) */
-    let depth: UInt32
+    var depth: UInt32
         /* Depth of image (number of samples in each tuple). */
-    let maxVal: Sample  /* Maximum defined value for a sample */
-    let bytes_per_sample: UInt32
+    var maxVal: Sample  /* Maximum defined value for a sample */
+    var bytes_per_sample: UInt32
         /* Number of bytes used to represent each sample in the image file.
            Note that this is strictly a function of 'maxval'.  It is in a
            a separate member for computational speed.
         */
-    let tuple_type: [CChar] /* [256] */
+    var tuple_type: String
         /* The tuple type string from the image header.  If the PAM image
            is really a view of a PBM, PGM, or PPM image, the value is
            PAM_PBM_TUPLETYPE, PAM_PGM_TUPLETYPE, or PAM_PPM_TUPLETYPE,
@@ -87,7 +123,7 @@ struct Pam {
 
            0 means the allocation depth is the same as the image depth.
         */
-    let comment: String
+    var comment: String
         /* Pointer to a pointer to a NUL-terminated ASCII string of
            comments.  When reading an image, this contains the
            comments from the image's PAM header; when writing, the
@@ -102,30 +138,30 @@ struct Pam {
            libnetpbm does not return comments and does not allocate any
            storage.
         */
-    let visual: Bool  /* boolean */
+    var visual: Bool  /* boolean */
         /* tuple_type is one of the PAM-defined tuple types for visual
            images ("GRAYSCALE", "RGB_ALPHA", etc.).
         */
-    let color_depth: UInt32
+    var color_depth: UInt32
         /* Number of color planes (i.e. 'depth', but without transparency).
            The color planes are the lowest numbered ones.  Meaningless if
            'visual' is false.
         */
-    let have_opacity: Bool   /* boolean */
+    var have_opacity: Bool   /* boolean */
         /* The tuples have an opacity (transparency, alpha) plane.
            Meaningless if 'visual' is false.
         */
-    let opacity_plane: UInt32
+    var opacity_plane: UInt32
         /* The plane number of the opacity plane;  meaningless if
            'haveOpacity' is false or 'visual' is false.
         */
-    let is_seekable: Bool  /* boolean */
+    var is_seekable: Bool  /* boolean */
         /* The file 'file' is seekable -- you can set the position of next
            reading or writing to anything and any time.
 
            If libnetpbm cannot tell if it is seekable or not, this is false.
         */
-    let raster_pos: pm_filepos
+    var raster_pos: _pm_filepos
         /* The file position of the raster (which is also the end of the
            header).
 
@@ -138,13 +174,13 @@ struct Pam {
         len = 0
         file = UnsafeMutablePointer<FILE>.allocate(capacity: 1)
         format = 0
-        plainformat = 0
+        plainformat = false
         height = 0
         width = 0
         depth = 0
         maxVal = 0
         bytes_per_sample = 0
-        tuple_type = [CChar](repeating: 0, count: 256)
+        tuple_type = ""
         allocation_depth = 0
         comment = ""
         visual = false
